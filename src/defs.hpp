4adb976727aeb781be6ba889ed33b2887296c38a
Xuzheng77s <xuzheng77@outlook.com>
2020-11-27 23:59:17 +0800

    ababab

--- src/defs.hpp
+++ src/defs.hpp
@@ -2,11 +2,53 @@
 #define LAUNCHER_DEFS_H
 
 #define LAUNCHER_VERSION 0.1
-#include <iostream>
+#define EXIT_SUCCESFUL 0
+#define EXIT_ERROR 1
+#define EXIT_UNEXPECTED -1
+#define PST_BEGIN namespace pst{
+#define PST_END }
 
-void cutline()
-{
-  std::cout << "-------------------------------------" << std::endl;
-}
+#include <iostream>
+using namespace std;
+
+PST_BEGIN
+  void cutline()
+  {
+    cout << "-------------------------------------" << endl;
+  }
+
+  int start server(const char* script_src)
+  {
+    int exit_code=system(script_src);
+    if(exit_code==0)
+    {
+      int c;
+      cout<<"退出成功,是否重启?(0)重启,(1)退出"<<endl;
+      cin>>c;
+      if(c==0)
+      {
+        start_server(script_src);
+      }
+      else
+      {
+        return EXIT_SUCCESFUL;
+      }
+    }
+    else
+    {
+      int c;
+      cout<<"退出失败,是否重启?(0)重启,(1)退出"<<endl;
+      cin>>c;
+      if(c==0)
+      {
+        start_server(script_src);
+      }
+      else
+      {
+        return exit_code;
+      }
+    }
+  }
+PST_END
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,23 @@
 #include <cstdlib>
 #include "defs.hpp"
 using namespace std;
+using namespace pst;
 
 int main(int argc, char const *argv[])
 {
   cout<<"欢迎来到服务器启动器 版本v"<<LAUNCHER_VERSION<<endl;
-  return 0;
+  cutline();
+  int choose;
+  const char src="./start.sh";
+  cout<<"请选择要做的事情:(1)启动服务器,(2)退出,(3)回滚纠错"<<endl;
+  switch (choose)
+  {
+    case 1:
+    start_server();
+    break;
+
+    case 2:
+    return EXIT_SUCCESFUL
+  }
+  return EXIT_SUCCESFUL;
 }
