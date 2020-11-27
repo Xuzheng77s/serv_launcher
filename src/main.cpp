d7dc7316ac1d4633a2b7310f65548db03fae68b8
Xuzheng77s <xuzheng77@outlook.com>
2020-11-27 23:44:03 +0800

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <assert>
+#include <cstdlib>
 #include "defs.hpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  cout<<"欢迎来到服务器启动器 版本v"<<LAUNCHER_VERSION<<endl;  
+  cout<<"欢迎来到服务器启动器 版本v"<<LAUNCHER_VERSION<<endl;
   return 0;
 }
